21dae2f5430a53563a624a2c5c5ce33530221294
Akhirwan Novendi <56789259+akhirwan@users.noreply.github.com>
2025-02-12 03:58:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember(){
+    var skills = ['JavaScript', 'React', 'Node', 'HTML', 'CSS', 'SQL'];
+    var member = 'John Doe';
+    console.log(member + ' has the following skills: ' + skills);
+}
